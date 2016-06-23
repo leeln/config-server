66e0a800119a5a4aec70dc723f16a16c8eba89cd
liuzhenyuan <liuzhenyuan@meituan.com>
2016-06-23 20:54:57 +0800

    添加脚本

---
+++ scripts/deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+cd $(dirname $0)
+
+cd ../
+
+TAG=${TAG:-$(awk '/ENV VERSION/{print $3}' Dockerfile)}
+NAME=${NAME:-$(awk '/ENV SERVER_NAME/{print $3}' Dockerfile)}
+PORT=${PORT:-$(awk '/ENV PORT/{print $3}' Dockerfile)}
+
+IMAGE=registry.gitlab.com/micoa/${NAME}:${TAG}
+
+docker stop docker ${NAME}
+
+docker rm ${NAME}
+
+docker run -d --name ${NAME} -p ${PORT}:${PORT} ${IMAGE} .
+
+echo Done Deploying ${IMAGE}
