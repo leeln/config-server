e9f9d9b275b3ca92389958a732de7069edcb4dbb
javy-liu <lzy7750015@gmail.com>
2016-08-17 17:13:59 +0800

    add

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-[![build status](https://gitlab.com/micoa/config-server/badges/master/build.svg)](https://gitlab.com/micoa/config-server/commits/master)
 # Config Server
--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -10,7 +10,7 @@ cd ../
 TAG=${TAG:-$(awk '/ENV VERSION/{print $3}' Dockerfile)}
 NAME=${NAME:-$(awk '/ENV SERVER_NAME/{print $3}' Dockerfile)}
 
-IMAGE=registry.gitlab.com/micoa/${NAME}:${TAG}
+IMAGE=docker.repo.waimai.sankuai.info/micoa/${NAME}:${TAG}
 
 docker build -t ${IMAGE} .
 
